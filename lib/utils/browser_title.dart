94949732850ba280f425f98e7abea5129785204b
a.ushkov <a.ushkov@jetmail.cc>
2025-12-22 13:27:12 +0300

    fix: change dart:html to package:web

--- lib/utils/browser_title.dart
+++ lib/utils/browser_title.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
-import 'dart:html' as html if (dart.library.io) 'dart:io';
+import 'package:web/web.dart' as web;
 
 class BrowserTitle {
-
   static void set(String title) {
     if (kIsWeb) {
-      html.document.title = title;
+      web.document.title = title;
     }
   }
 
   static String get() {
     if (kIsWeb) {
-      return html.document.title;
+      return web.document.title;
     }
     return '';
   }
